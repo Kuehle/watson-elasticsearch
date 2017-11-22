a919e1c24d9d53d1eaa0f50b592cae4efd67c37c
Johannes Kühlewindt <johannes.kuehlewindt@bytabo.de>
2017-11-22 22:32:00 +0100

    improved sample query

--- query-test.js
+++ query-test.js
@@ -4,8 +4,19 @@ const client = new elasticsearch.Client({
     log: 'trace'
 })
 
-client.search({
-    q: 'chicken'
-}).then((body) => {
-    console.log("body", body.hits.hits)
-}, (err) => console.trace(error.message))
+
+function search(searchObj) {
+    return new Promise((resolve, reject) => {
+        client.search(searchObj).then((body) => {
+            resolve(body.hits.hits)
+        }, (err) => {
+            console.trace(error.message)
+            reject(err)
+        })
+    })
+}
+
+// sample query for category search
+// https://www.elastic.co/guide/en/elasticsearch/client/javascript-api/current/api-reference.html#api-search
+// extended body searches https://www.elastic.co/guide/en/elasticsearch/reference/6.0/search-request-body.html 
+search({"q" : "category:beef", "size": "2", "sort": "name:-1,area:1"}).then(results => console.log("Category Beef:", results))
